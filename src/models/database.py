7754af9f6e3357f4fc6d418a1cccc54e2bc5ec8f
Quan Dang <18520339@gm.uit.edu.vn>
2024-03-18 00:49:59 +1100

    Refactor database file path in Database class

--- src/models/database.py
+++ src/models/database.py
@@ -5,27 +5,27 @@ from models import Student
 
 
 class Database:
-    def __init__(self, file_name: str = 'students.data'):
-        self._file_name = file_name
+    def __init__(self, db_path: str = 'common/students.data'):
+        self._db_path = db_path
         self._check_file_exists()
     
     @property
-    def file_name(self):
-        return self._file_name
+    def db_path(self):
+        return self._db_path
 
 
     def _check_file_exists(self) -> None:
         # Checks if a file exists, creates it if not
-        if not os.path.exists(self.file_name):
-            with open(self.file_name, 'wb') as file:
+        if not os.path.exists(self.db_path):
+            with open(self.db_path, 'wb') as file:
+                print(f'No data file found => Created new data file: {self.db_path}')
                 pickle.dump([], file)
-                print(f'No data file found => Created new data file: {self.file_name}')
 
 
     def load_students(self) -> List[Student]:
         # Read and return the list of student objects from the data file
         self._check_file_exists()
-        with open(self.file_name, 'rb') as file:
+        with open(self.db_path, 'rb') as file:
             return pickle.load(file)
 
 
@@ -38,7 +38,7 @@ class Database:
                 break
         else: students.append(student)
 
-        with open(self.filename, 'wb') as file:
+        with open(self.db_path, 'wb') as file:
             pickle.dump(students, file)
 
 
@@ -48,7 +48,7 @@ class Database:
         for student in students:
             if student.student_id == student_id:
                 students.remove(student)
-                with open(self.filename, 'wb') as file:
+                with open(self.db_path, 'wb') as file:
                     pickle.dump(students, file)
                 return True
         return False
@@ -56,6 +56,6 @@ class Database:
     
     def clear_data(self) -> None:
         # Clears all records from the data file
-        with open(self.filename, 'wb') as file:
+        with open(self.db_path, 'wb') as file:
             pickle.dump([], file)
             print('All student records have been cleared.')
